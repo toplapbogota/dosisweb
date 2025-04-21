8014d1654ea4dfc4fe0c519fa3b99bfff34f81b3
camiloNemocon <cnemocon@movilidadbogota.gov.co>
2025-04-20 20:05:34 -0500

    creación de arreglo de funciones de osc

--- src/osc/tunneling.js
+++ src/osc/tunneling.js
@@ -14,7 +14,7 @@ let valor;
 let callback;
 let configuration;
 
-
+let funciones=[];
 
 
 
@@ -33,29 +33,42 @@ function activarOSC(nombreCanal, puertoRecibir) {
             mensajeControl = msg.address.split("/")[2];
 
             console.log("Mensaje : ", mensajeControl);
+
+            console.log(funciones);
             
-            
-            if(mensajeControl === control && escuchar)
-            {
-                if(callback == "prender")
+            for(let i = 0; i < funciones.length; i++) {
+                
+                let funcion = funciones[i];
+
+                control = funcion.control;
+                callback = funcion.callback;
+                valor = funcion.valor;
+                configuration = funcion.configuration;
+
+                //if(mensajeControl === control && escuchar)
+                if(mensajeControl === control)
                 {
-                    prender(valor);
-                }
-                else if(callback == "apagar")
-                {
-                    apagar(valor);
-                }
-                else if(callback == "servo")
-                {
-                    servo(configuration);
-                }
-                else if(callback == "apagarServos")
-                {
-                    apagarServos();
+                    console.log("Mensaje : ", mensajeControl, " Control : ", control, " Valor : ", valor, " Callback : ", callback );
+                    if(callback == "prender")
+                    {
+                        prender(valor);
+                    }
+                    else if(callback == "apagar")
+                    {
+                        apagar(valor);
+                    }
+                    else if(callback == "servo")
+                    {
+                        servo(configuration);
+                    }
+                    else if(callback == "apagarServos")
+                    {
+                        apagarServos();
+                    }
+    
                 }
 
             }
-               
 
         });   
     });    
@@ -65,10 +78,14 @@ global.activarOSC = activarOSC;
 
 function oscEscuchar(msgControl, msgValor, msgCallback, msgConfiguration) {
     console.log("oscEscuchar : ", control, valor);
-    escuchar = true;
+    //escuchar = true;
+    /*
     control = msgControl;
     valor = msgValor;
     callback = msgCallback;
     configuration = msgConfiguration;
+    */
+   funciones.push({control: msgControl, valor: msgValor, callback: msgCallback, configuration: msgConfiguration});
+   console.log("Funciones : ", funciones);
 }
 global.oscEscuchar = oscEscuchar;
