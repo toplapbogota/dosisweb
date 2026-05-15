0c4f87320758becc9d93a5a1fb5a4c5c9611459b
Alvaro O'Byrne <alvarobyrne@gmail.com>
2026-05-15 00:52:15 -0500

    Use the themes controller in the ui

--- src/ui/gui.js
+++ src/ui/gui.js
@@ -1,9 +1,27 @@
-import GUI from 'lil-gui'; 
+import GUI from 'lil-gui';
+
 import init from '../init.js';
+
+import ThemeController from '../editor/themes/themesController.js';
+
 const gui = new GUI();
 
-gui.add(window.location,'reload').name('recargar')
+gui.add(window.location, 'reload').name('recargar');
 
-gui.add({init},'init').name('iniciar')
+gui.add({ init }, 'init').name('iniciar');
 
-export default gui
+export function setupThemeController(editor) {
+  const themeController = new ThemeController(editor, gui);
+
+  themeController.on('themeChanged', (theme) => {
+    console.log(
+      '%c Theme changed to:',
+      'color: #0e93e0;background: #aaefe5;',
+      theme
+    );
+  });
+
+  return themeController;
+}
+
+export default gui;
