031f8aefab0969e0530149517eea0db596d329bd
Alvaro O'Byrne <alvarobyrne@gmail.com>
2026-05-15 00:53:38 -0500

    Set the thmes controller in the app

--- src/app.js
+++ src/app.js
@@ -4,11 +4,12 @@ import './polyfills/hrtime'
 import Editor from './editor'
 import handleEval from './evaluation'
 import storedObject from './session'
+import { setupThemeController } from './ui/gui'
 
 
 const editorTextArea = document.querySelector('textarea')
 
 const editor = new Editor(editorTextArea)
 storedObject && editor.cm.setValue(storedObject.code)
-
+const themeController = setupThemeController(editor.cm)
 editor.on('eval', handleEval)
