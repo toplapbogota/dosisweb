d57449ffb7c90b02d369d030551c9b1e420595d0
Alvaro O'Byrne <alvarobyrne@gmail.com>
2026-08-28 09:04:41 -0500

    Listen to the event at the app level

--- src/app.js
+++ src/app.js
@@ -5,13 +5,14 @@ import './camera/index'
 import Editor from './editor'
 import handleEval from './evaluation'
 import { appStorage } from './storage/appStorage'
-import { setupThemeController } from './ui/gui'
+import gui,{ setupThemeController } from './ui/gui'
 
 
 const editorTextArea = document.querySelector('textarea')
 
 const editor = new Editor(editorTextArea)
 editor.on('eval', handleEval)
+editor.on('hide-all', isVisible => { isVisible ? gui.show() : gui.hide() })
 
 const storedCode = appStorage.getItem('code');
 storedCode && editor.cm.setValue(storedCode)
